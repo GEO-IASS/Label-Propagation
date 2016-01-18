2509629665475f78a6051d2de267fe4077f0074e
Pallab Maji <pallab.vsp@gmail.com>
2016-01-18 22:01:34 +0530

    Function to visualize and docs added

--- code/parameters.m
+++ code/parameters.m
@@ -12,4 +12,4 @@ file_names{3} =  {'LabelMeVideo'}; %LabelMe data
 data_path = '../data/'; %path where the actual data is
 
 numsel_frames = [5 10]; %Number of frames to be annotated
-dbids = 2; file_id = 1; %This means use girl sequence from segtrack data
+dbids = 1; file_id = 6; %This means use girl sequence from segtrack data
